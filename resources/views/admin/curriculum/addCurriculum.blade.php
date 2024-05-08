225d370178fbd3745791bad98706902534969e15
Mustafa Gamal <mustafagamal35@gmail.com>
2024-05-08 11:36:11 +0300

    edit

--- resources/views/admin/curriculum/addCurriculum.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('admin.layout')
-
-@section('Title')
-    Add Curriculum
-@endsection
-
-@section('Body')
-Add Curriculum
-@endsection
--- resources/views/errors.blade.php
+++
@@ -1,9 +0,0 @@
-@if ($errors->any())
-    @foreach ($errors->all() as $error)
-        <div class="container">
-            <div class="alert alert-danger text-center">
-                <i class="fas fa-times"></i> {{$error}}
-            </div>
-        </div>
-    @endforeach
-@endif
